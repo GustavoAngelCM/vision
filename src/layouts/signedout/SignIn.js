b05acf5a07498c9631bee65b96d08507b6114b8c
GustavoACM <crgt7_8@hotmail.com>
2020-05-19 13:07:49 -0400

    integrate electron and react js => customize material-ui and google fonts

---
+++ src/layouts/signedout/SignIn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const SignIn = () => (
+    <Button variant="contained" color="primary">
+      Hola Mundito!
+    </Button>
+);
+
+export default SignIn;
